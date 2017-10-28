e55e5bacfb6c88b097a06ee7937eb99fce78a2bf
Risa Ezoe <jolie@pd-mba02.local>
2017-10-28 17:26:52 +0900

    tabbarのいろいろ設定した

--- Availee-iOS/Availee-iOS/ViewController/FriendViewController.swift
+++ Availee-iOS/Availee-iOS/ViewController/FriendViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class FriendViewController: UIViewController {
 
-    
+    var id: Int = 0
+    var icon: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,15 +23,4 @@ class FriendViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
