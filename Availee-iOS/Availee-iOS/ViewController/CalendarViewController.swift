917473844298cdbd2567cb79a73e9b37bc2afecb
Kazuki Kanamaru <kazuki@KazukinoMacBook-Pro.local>
2017-10-28 17:44:49 +0900

    予定がある日付の着色表示

--- Availee-iOS/Availee-iOS/ViewController/CalendarViewController.swift
+++ Availee-iOS/Availee-iOS/ViewController/CalendarViewController.swift
@@ -22,22 +22,36 @@ class CalendarViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func koyomiSetup() {
-        koyomi.dayPosition = .topLeft
-    }
-  
     
+    func koyomiSetup() {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        koyomi.calendarDelegate = self
+        let today = Date()
+        
+        koyomi.dayPosition = .topLeft
+        koyomi.selectionMode = .multiple(style: .line)
+        koyomi.holidayColor = (.black,.black) // = (saturday: UIColor.black, sunday: UIColor.black)
+        koyomi.lineView.height = 10
+        koyomi.lineView.position = .bottom
+        
     }
-    */
+    
+    
+}
 
+extension CalendarViewController: KoyomiDelegate {
+    
+    func koyomi(_ koyomi: Koyomi, selectionTextColorForItemAt indexPath: IndexPath, date: Date) -> UIColor? {
+        return #colorLiteral(red: 0.06666666667, green: 0.1215686275, blue: 0.2, alpha: 1)
+
+    }
+    
+    func koyomi(_ koyomi: Koyomi, selectionColorForItemAt indexPath: IndexPath, date: Date) -> UIColor? {
+        return #colorLiteral(red: 0.137254902, green: 0.8666666667, blue: 0.6980392157, alpha: 1)
+    }
+    
+    
+    
 }
 
 
