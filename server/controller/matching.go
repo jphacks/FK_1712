ee3289b764a1375d1fdb4c5fb75cb6374b1aaac1
valai <goriakku@gmail.com>
2017-10-29 11:24:24 +0900

    カレンダー情報を毎回削除して、格納するよう変更

--- server/controller/matching.go
+++ server/controller/matching.go
@@ -6,6 +6,7 @@ import (
 	"../model"
 	"../db"
 	"time"
+	"fmt"
 )
 
 func FindMatching(c *gin.Context) {
@@ -23,23 +24,30 @@ func FindMatching(c *gin.Context) {
 
 	c.BindJSON(cal)
 
+	res, err := db.Sess.DeleteFrom("free_time").Where("user_id = ?", cal.UserID).Exec()
+	if err != nil {
+		fmt.Println(err)
+	}else{
+		fmt.Println(res)
+	}
 	for _, t := range cal.FreeTime {
 		db.Sess.InsertInto("free_time").Columns("user_id", "start_time", "end_time").Values(cal.UserID, t.StartTime, t.EndTime).Exec()
 	}
 
-	db.Sess.Select("*").From("friends").Where("follow_id = ?", cal.UserID).Load(&conn)
-
+	db.Sess.DeleteFrom("free_date").Where("user_id = ?", cal.UserID).Exec()
 	for _, d := range cal.FreeDate {
 		db.Sess.InsertInto("free_date").Columns("user_id", "start_date", "end_date").Values(cal.UserID, d.StartDate, d.EndDate).Exec()
 	}
 
+	db.Sess.Select("*").From("friends").Where("follow_id = ? OR follower_id = ?", cal.UserID, cal.UserID).Load(&conn)
+
 	for _, t := range cal.FreeTime {
-		start_time, _ := time.Parse("2006-01-02 15:04:05 MST", t.StartTime)
-		end_time, _ := time.Parse("2006-01-02 15:04:05 MST", t.EndTime)
+		start_time, _ := time.Parse("2006/01/02 15:04:05 MST", t.StartTime)
+		end_time, _ := time.Parse("2006/01/02 15:04:05 MST", t.EndTime)
 		for _, con := range conn {
 			db.Sess.Select("*").From("free_time").Where("user_id = ?", con.FollowerID).Load(&free_t)
-			start_time_tag, _ := time.Parse("2006-01-02 15:04:05 MST", free_t.StartTime)
-			end_time_tag, _ := time.Parse("2006-01-02 15:04:05 MST", free_t.EndTime)
+			start_time_tag, _ := time.Parse("2006/01/02 15:04:05 MST", free_t.StartTime)
+			end_time_tag, _ := time.Parse("2006/01/02 15:04:05 MST", free_t.EndTime)
 			if start_time.Sub(start_time_tag) < 0 {
 				start_time_cal = start_time_tag
 			}else{
@@ -52,7 +60,11 @@ func FindMatching(c *gin.Context) {
 			}
 
 			if end_time_cal.Sub(start_time_cal) >= 3 * time.Hour {
-				mat.UserID = con.FollowerID
+				if cal.UserID != con.FollowerID {
+					mat.UserID = con.FollowerID
+				}else{
+					mat.UserID = con.FollowID
+				}
 				mat.StartDate = start_time_cal
 				mat.EndDate = end_time_cal
 				mat.IsDate = false
@@ -62,12 +74,15 @@ func FindMatching(c *gin.Context) {
   }
 
 	for _, t := range cal.FreeDate {
-		start_date, _ := time.Parse("2006-01-02", t.StartDate)
-		end_date, _ := time.Parse("2006-01-02", t.EndDate)
+		start_date, _ := time.Parse("2006/01/02", t.StartDate)
+		end_date_pre, _ := time.Parse("2006/01/02", t.EndDate)
+		end_date := end_date_pre.Add(24 * time.Hour)
 		for _, con := range conn {
 			db.Sess.Select("*").From("free_date").Where("user_id = ?", con.FollowerID).Load(&free_d)
-			start_date_tag, _ := time.Parse("2006-01-02", free_d.StartDate)
-			end_date_tag, _ := time.Parse("2006-01-02", free_d.EndDate)
+			start_date_tag, _ := time.Parse("2006/01/02", free_d.StartDate)
+			end_date_tag_pre, _ := time.Parse("2006/01/02", free_d.EndDate)
+			end_date_tag := end_date_tag_pre.Add(24 * time.Hour)
+
 			if start_date.Sub(start_date_tag) < 0 {
 				start_date_cal = start_date_tag
 			}else{
@@ -80,9 +95,13 @@ func FindMatching(c *gin.Context) {
 			}
 
 			if end_date_cal.Sub(start_date_cal) >= 24 * time.Hour {
-				mat.UserID = con.FollowerID
+				if cal.UserID != con.FollowerID {
+					mat.UserID = con.FollowerID
+				}else{
+					mat.UserID = con.FollowID
+				}
 				mat.StartDate = start_date_cal
-				mat.EndDate = end_date_cal
+				mat.EndDate = end_date_cal.Add(-24 * time.Hour)
 				mat.IsDate = true
 				mats = append(mats, *mat)
 			}
--- server/controller/users.go
+++ server/controller/users.go
@@ -1,15 +1,33 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
 	"../model"
 	"../db"
+	"fmt"
+	"time"
+	"net/http"
+	"crypto/sha1"
+
+	"github.com/gin-gonic/gin"
+//	"github.com/gin-gonic/contrib/static"
+//	"github.com/mattn/go-colorable"
+	"github.com/olahol/go-imageupload"
 )
 
 func InsertUser(c *gin.Context) {
 	u := new(model.UsersJSON)
 	c.BindJSON(u)
+
+  img, err := imageupload.Process(c.Request, "file")
+  if err != nil {
+      panic(err)
+  }
+  thumb, err := imageupload.ThumbnailPNG(img, 300, 300)
+  if err != nil {
+      panic(err)
+  }
+  h := sha1.Sum(thumb.Data)
+  thumb.Save(fmt.Sprintf("files/%s_%x.png", time.Now().Format("20060102150405"), h[:4]))
 
 	db.Sess.InsertInto("users").Columns("name", "icon").Values(u.Name, u.Icon).Exec()
 
@@ -25,5 +43,4 @@ func SearchUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response_user)
 }
-
 
